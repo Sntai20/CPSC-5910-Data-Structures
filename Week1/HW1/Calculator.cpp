a730b19f82533a57c27c6794312779c8050c6bbf
Antonio Santana <ansantan@microsoft.com>
2021-04-04 11:59:10 -0700

    Working on calc implementation

--- Week1/HW1/Calculator.cpp
+++ Week1/HW1/Calculator.cpp
@@ -1,1 +1,41 @@
 // TOOO: Calculator implementation goes in this file.
+
+#include "Calculator.h"
+
+// Adds two numbers and returns the result.
+double Calculator::Add(const double first, const double second){
+    double results = 0;
+    op = '+';
+    double Calc(const double first, const double second, const char op);
+
+    return results;
+}
+
+
+// Subtracts two numbers and returns the result.
+double Calculator::Subtract(const double first, const double second){
+    double results = 0;
+    results = first - second;
+    return results;
+}
+
+// Multiplies two numbers and returns the result.
+double Calculator::Multiply(const double first, const double second){
+    double results = 0;
+    results = first * second;
+    return results;
+}
+
+// Divides two numbers and returns the result.
+double Calculator::Divide(const double first, const double second){
+    double results = 0;
+    results = first / second;
+    return results;
+}
+
+// Runs the given calculation and returns the result.
+double Calculator::Calc(const double first, const double second, const char op){
+    const double results = 0;
+    results = first op second;
+    return results;
+}
--- Week1/HW1/CalculatorIO.cpp
+++ Week1/HW1/CalculatorIO.cpp
@@ -1,1 +1,23 @@
 // TOOO: CalculatorIO implementation goes in this file.
+#include "CalculatorIO.h"
+
+// Reads calculations line by line from input file and writes the results line by line to the output file.
+// If an error occurs on any given calculation, the output should include:
+//  1. A description of the error.
+//  2. An indication of what line number the error occurred on.
+//  3. And the method should continue processing the rest of the lines in the file.
+//
+// Example input file:
+// 7 * 8
+// 24 / 4
+//
+// Example output file:
+// 56
+// 6
+void CalculatorIO::CalculateFile(const std::string& inputFilePath, const std::string& outputFilePath){
+
+}
+
+double CalculatorIO::CalculateInteractive(){
+
+}
