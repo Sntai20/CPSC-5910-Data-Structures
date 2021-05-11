c30c6d8b1ecac5560e94d4155dd9e7d4685cfd3f
Antonio Santana <ansantan@microsoft.com>
2021-05-10 17:28:43 -0700

    Updated comments and added plot

--- Week5/HW5/Hydra.cpp
+++ Week5/HW5/Hydra.cpp
@@ -12,11 +12,31 @@ using namespace std;
  * The hydraTester prompts the user to enter a starting string. To make hydra, the string
  * is then stored in a list one character at a time. While Hydra has heads, the program counts
  * the number of heads that the hero ends up cutting off to defeat.
+ *
+ * The time requirements for the algorithm is O(n*2^n). The number of operations consist of traversing
+ * the list to read, add, and remove from the list. The hydraTester reads in the starting string to
+ * make hydra, each add operation is O(n). Once the word is stored into a list the hydraTester performs a
+ * couple of operations inside of a loop. To simulate the head being cut off and growing two heads requires
+ * the first letter of the string to be removed, while two add operations O(n^2) are performed to make two
+ * copies of the string and add them into the list. The following output shows the algorithm growing exponentially
+ * as the length of the strings increases from 13 to 31.
+ *
+ * Runtime	Length of Strings	Heads cut off	Starting Strings
+ * 5153	    9	                   511	        stringwit
+ * 7064	    13	                   8191	        stringwithaaa
+ * 15251	16	                   65535	    stringwithaaabbb
+ * 21573	19	                   524287	    stringwithaaabbbccc
+ * 22003	22	                   4194303	    stringwithaaabbbcccddd
+ * 32517	25	                   33554431	    stringwithaaabbbcccdddeee
+ * 103836	28	                   268435455	stringwithaaabbbcccdddeeefff
+ * 740088	31	                   2147483647	stringwithaaabbbcccdddeeefffggg
+ *
  */
+
 void hydraTester()
 {
     LinkedList<string>* hydra = new LinkedList<string>();
-    cout << "Enter a starting string to represents Hydra, a monster with many heads" << endl;
+    cout << "Enter a starting string to represent Hydra, a monster with many heads" << endl;
     string startingString;
     cin >> startingString;
 
@@ -39,7 +59,7 @@ void hydraTester()
             hydra->insert(1, smallerHydraHead);
             hydra->insert(1, smallerHydraHead);
         }
-    } // O(n)
+    } // O(n*2^n)
 
     cout << "\nTo defeat the Hydra Monster, the hero cut off " << count << " heads " ;
 }
