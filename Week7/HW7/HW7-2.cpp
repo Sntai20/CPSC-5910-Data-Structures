2016cfc5ab0928760af01881c1b362eaa060a268
Antonio Santana <antonio.santana.ir@outlook.com>
2021-05-24 07:30:37 -0700

    Not done, started to parse input

--- Week7/HW7/HW7-2.cpp
+++ Week7/HW7/HW7-2.cpp
@@ -17,20 +17,222 @@
  * in a file called HW7-2.cpp and compile with the standard command-line flags.
  */
 #include <iostream>
+#include<vector>
+#include<string>
+#include<fstream>
+using namespace std;
+
+//Function prototypes
+template <typename T>
+void sort(vector<T>& data);
+template <typename T>
+void display(vector<T> data);
+
+int main()
+{
+    string filename1, filename2,data;
+    bool isInt = true;
+//Prompt for file names
+    cout << "Enter file names with space: ";
+    cin >> filename1 >> filename2;
+    ifstream in1(filename1);
+    ifstream in2(filename2);
+//File open error check
+    if (!in1 || !in2) {
+        cout << "File not found\n";
+        exit(0);
+    }
+//Check for integer
+    in1 >> data;
+    for (int i = 0; i < data.length(); i++)
+    {
+        if (!isdigit(data[i])) {
+            isInt = false;
+            break;
+        }
+    }
+//If integer read into a int vector
+    if (isInt) {
+        vector<int> values;
+        values.push_back(stoi(data));
+        while (in1 >> data) {
+            values.push_back(stoi(data));
+        }
+        while (in2 >> data) {
+            values.push_back(stoi(data));
+        }
+        in1.close();
+        in2.close();
+//Display before sort
+        cout << "Values before sort:\n";
+        display(values);
+//Call sort
+        sort(values);
+//Display after sort
+        cout << "\nValues after sort:\n";
+        display(values);
+    }
+//String data
+    else {
+        vector<string> values;
+        values.push_back(data);
+        while (in1 >> data) {
+            values.push_back(data);
+        }
+        while (in2 >> data) {
+            values.push_back(data);
+        }
+        in1.close();
+        in2.close();
+        cout << "Values before sort:\n";
+        display(values);
+        sort(values);
+        cout << "\nValues after sort:\n";
+        display(values);
+    }
+    return 0;
+}
+
+/*
+* This is a template function
+* So passed argument data type not matter
+* Sort using insertion sort
+* Complexity become O(n)
+*/
+
+template <typename T>
+void sort(vector<T>& data) {
+    for (int i = 1; i < data.size(); i++)
+    {
+        T key = data[i];
+        int j = i - 1;
+        while (j >= 0 && data[j] > key)
+        {
+            data[j + 1] = data[j];
+            j = j - 1;
+        }
+        data[j + 1] = key;
+    }
+}
+/*
+* Display passed data
+*/
+template <typename T>
+void display(vector<T> data)
+{
+    for (int i = 0; i < data.size(); i++) {
+        cout << data[i] << endl;
+    }
+}
+
+/*
+Output
+
+        Enter file names with space: strfile1.txt strfile2.txt
+        Values before sort:
+hello
+        what
+you
+        want
+hi
+        how
+are
+        you
+
+Values after sort:
+are
+        hello
+hi
+        how
+want
+        what
+you
+        you
+
+*/
+/*
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <cctype>
 #include <list>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
-void hW7Tester(string userInput);
-bool fileExists(string userInput);
-//template<typename ItemType>
-//vector<ItemType> preProcess(string &inputFilePath);
-template<typename ItemType>
-list<ItemType> preProcess(string &inputFilePath);
+bool fileExists(string userInput)
+{
+    ifstream ifile;
+    ifile.open(userInput);
+    if(ifile) { return true; }
+    else return false;
+}
+
+
+// Function to merge the two halves vector left and vector right of the vector vec.
+void merge (vector<string>& vec, vector<string>& left, vector<string>& right) {
+    // Create iterators for input vectors
+    vector<string>::iterator v = vec.begin();
+    vector<string>::iterator l = left.begin();
+    vector<string>::iterator r = right.begin();
+
+    while (l != left.end() && r != right.end())
+    {
+        if (*l <= *r ){
+            *v = *l;
+            l++;
+        }
+        else {
+            *v = *r;
+            r++;
+        }
+        v++;
+    }
+    // Copy remaining elements of vector left, if there are any
+    while (l != left.end())
+    {
+        *v = *l;
+        l++;
+        v++;
+    }
+
+    // Copy remaining elements of vector right, if there are any
+    while (r != right.end())
+    {
+        *v = *r;
+        r++;
+        v++;
+    }
+}
+
+// Function to merge sort a vector of integers. Accepts a reference to the vector.
+void mergeSort (vector<string> &vec)
+{
+    if (vec.size() == 1) return;
+    else {
+
+        // Declare temp vectors
+        vector<string> left;
+        vector<string> right;
+
+        int mid = (vec.size() + 1) / 2;
+
+        // Copy contents of input vector to temp vectors
+        for (int i=0; i < mid; i++){
+            left.push_back(vec[i]);
+        }
+
+        for (int i = mid; i < (int)vec.size(); i++) {
+            right.push_back(vec[i]);
+        }
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(vec, left, right);
+
+    }
+}
 
 int main() {
     string userInput;
@@ -45,90 +247,93 @@ int main() {
 
     // Prints the k'th largest integer in the file.
     cout << "HW7 tester" << endl << "============" << endl;
-    hW7Tester(userInput);
 
-    return 0;
-}
-
-bool fileExists(string userInput)
-{
-    ifstream ifile;
-    ifile.open(userInput);
-    if(ifile) { return true; }
-    else return false;
-}
-
-//template<typename ItemType>
-//vector<int> preProcess(ItemType &inputFilePath)
-//{
-//    ifstream inputFile;
-//    int number;
-//    inputFile.open(inputFilePath);
-//    vector<int> numbers;
-//
-//    while(inputFile >> number)
-//    {
-//        numbers.push_back(number);
-////                cout << "Added " << number << " to number vector. " << endl;
-//    }
-//    inputFile.close();
-//    return numbers;
-//}
-
-template<typename ItemType>
-list<int> preProcess(ItemType &inputFilePath)
-{
-    ifstream inputFile;
-    int number = 0;
-    inputFile.open(inputFilePath);
-    list<int> numbers;
-
-    while(inputFile >> number)
-    {
-        numbers.push_back(number);
-    }
-
-    inputFile.close();
-    return numbers;
-}
-
-void hW7Tester(string userInput) {
     bool foundException = false;
     try
     {
         // Create a test file
-        srand(7);
-
-        const int RANGE = 1000;
-        const long SIZE = 10000;
-        int* data = new int[SIZE];
-        for (int i = 0; i < SIZE; i++)
-        {
-            data[i] = rand() % RANGE;
-        }
-
-        std::ofstream myfile;
-        myfile.open ("testints.txt");
-        for (int i = 0; i < SIZE; i++)
-        {
-            myfile << data[i] << endl;
-        }
-        myfile.close();
+//        srand(7);
+//
+//        const int RANGE = 1000;
+//        const long SIZE = 10000;
+//        int* data = new int[SIZE];
+//        for (int i = 0; i < SIZE; i++)
+//        {
+//            data[i] = rand() % RANGE;
+//        }
+//
+//        std::ofstream myfile;
+//        myfile.open ("testints.txt");
+//        for (int i = 0; i < SIZE; i++)
+//        {
+//            myfile << data[i] << endl;
+//        }
+//        myfile.close();
 
         string inputFilePath;
         if (fileExists(userInput) && userInput.length() >= 1)
         {
             inputFilePath = userInput;
+            vector<string> stringData;
+            vector<int> numbers;
+            ifstream inputFile;
+            string data;
+            inputFile.open(inputFilePath);
 
-//            vector<int> numbers = preProcess(inputFilePath);
-            list<int> numbers = preProcess(inputFilePath);
+//            int number = 0;
+//            while(inputFile >> number)
+//            {
+//                numbers.push_back(number);
+//            }
+            stringstream ss;
 
-            numbers.sort();
+
+//             Running loop till the end of the stream
+            string temp;
+            int found;
+
+            while(inputFile >> data)
+            {
+                 Storing the whole string into string stream
+                ss << data;
+//                 extracting word by word from stream
+                ss >> temp;
+
+//                 Checking the given word is integer or not
+                if (stringstream(data) >> found)
+                {
+                    numbers.push_back(found);
+                }
+
+                if (data.length() >= 1)
+                {
+                    for (long unsigned int i = 0; i < data.length(); i++)
+                    {
+                        char c = tolower(data[i]);
+                        if (isalpha(c))
+                        {
+                            stringData.push_back(data);
+                        }
+                    }
+                }
+
+//                 To save from space at the end of string
+                temp = "";
+            }
+            inputFile.close();
+
+            mergeSort(stringData);
+//            mergeSort(numbers);
+            for (auto sD :  stringData)
+            {
+                cout << sD << " ";
+
+            }
+            cout << endl;
             for (auto num :  numbers)
             {
-                cout << num << endl;
+                cout << num << " ";
             }
-            return;
         }
     }
     catch (invalid_argument& e)
@@ -140,4 +345,26 @@ void hW7Tester(string userInput) {
         cerr << "\nDid not catch an invalid input" << endl;
     }
 
+    return 0;
 }
+
+
+
+
+//template<typename ItemType>
+//list<int> preProcess(ItemType &inputFilePath)
+//{
+//    ifstream inputFile;
+//    int number = 0;
+//    inputFile.open(inputFilePath);
+//    list<int> numbers;
+//
+//    while(inputFile >> number)
+//    {
+//        numbers.push_back(number);
+//    }
+//
+//    inputFile.close();
+//    return numbers;
+//}
+*/
--- Week7/HW7/HW7-3.cpp
+++ Week7/HW7/HW7-3.cpp
@@ -7,6 +7,81 @@
  * Print the sorted array to the console. Your program should be fully contained in a file
  * called HW7-3.cpp and compile with the standard command-line flags.
  */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //declaring variable n
+    int n;
+    cout<<"Enter an integer n: ";
+    //taking the user input n
+    cin>>n;
+    //declaring an array of size n+1
+    int arr[n+1];
+    for(int i=1;i<=n;i++)
+    {
+        //Generating random integers in between 1 and 1000 and storing it inside arr[i]
+        arr[i]=(rand()%1000)+1;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+    //Initializing an array brr[] of size n+1 and an array of size 1001 which is crr[]
+    int brr[n+1],crr[1001]={0};
+    for(int i=1;i<=n;i++)
+    {
+
+        crr[arr[i]]+=1;
+    }
+
+    //Now crr[] array's each block contains the number of elements which are equal to its each index
+    for(int i=1;i<=1000;i++)
+    {
+        crr[i]=crr[i]+crr[i-1];
+    }
+    //now crr[] array's each block contains number of elements which are less than or equal to each index
+    for(int i=1;i<=n;i++)
+    {
+        //In the following line we are actually storing the element at index i of arr[]
+        // at index crr[arr[i]] of brr[] array because we know that crr[arr[i]]
+        //contains number of elements less than or equal to arr[i]
+        brr[crr[arr[i]]]=arr[i];
+        //At the same time we are decrementing crr[arr[i]] by 1 because we have
+        //alreday placed arr[i] in brr[] array in the perfect position
+        crr[arr[i]]=crr[arr[i]]-1;
+    }
+    //printing the brr[] array which contains the sorted elements of arr[] array in increasing order
+    for(int i=1;i<=n;i++)
+    {
+        cout<<brr[i]<<" ";
+    }
+
+    return 0;
+}
+
+/*
+Explanation:>
+
+I have chosen counting-sort algorithm because it's the best choice as it operates best on an array of size n whose elements are in the range
+
+of 1 to k.It's complexity is roughly O(n).It is also a stable algorithm.Here the given range is 1 to 1000.so k=1000
+
+Now ley us see the code working to calculate the time complexity of the algorithm .There are basically 2 types of for loops one is running from
+
+1 to n and another is running from 1 to 1000.
+
+So,overall time complexity is O(n+1000) ≃ O(n)
+
+Output:>
+
+Enter an integer n: 15 42 468 335 501 170 725 479 359 963 465 706 146 282 828 962 42 146 170 282 335 359 465 468 479 501 706
+*/
+
+
+/*
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -95,5 +170,34 @@ void hW7Tester() {
 
 }
 
+*/
 
+/*
+ * vector<int>* numbers;
+        if (!inFile) {
+            throw std::runtime_error("invalid input!");
+        }
+        cout << "** TEST INSERT**" << endl << "Inserting in this order: ";
+        string line;
 
+        vector<int>::iterator myPosition = numbers->begin();
+        while (getline(inFile, line)) {
+            stringstream ss(line);
+            int n;
+            ss >> n;
+            cout << n << " ";
+            numbers->insert(myPosition, n);
+        }
+
+        cout << endl;
+        auto start = std::chrono::high_resolution_clock::now();
+
+//        mergeSort(numbers, 0, SIZE - 1);
+        auto stop = std::chrono::high_resolution_clock::now();
+
+        for (auto number : *numbers)
+        {
+            cout << number << endl;
+        }
+
+ */
